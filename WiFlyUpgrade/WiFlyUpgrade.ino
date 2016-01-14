bb7eaa4e2fbaf7edc13d0037747d35975a858a41
MakerBro <x1sc0@acrobotic.com>
2016-01-14 14:33:58 -0800

    Update WiFlyUpgrade.ino

--- WiFlyUpgrade/WiFlyUpgrade.ino
+++ WiFlyUpgrade/WiFlyUpgrade.ino
@@ -2,6 +2,23 @@
   1. Send $$$ with no line ending
   2. Switch to carriage return
   3. Send commands
+  # Typically, scan needs to be run twice for it to work
+  scan 
+  #WPA2
+  set wlan auth 4
+  set wlan phrase my_password
+  join my_ssid
+  #FTP
+  set ftp user roving
+  set ftp password Pass123
+  set ftp directory public
+  set ftp filename wifly3-475.img
+  set ftp mode 1
+  set ftp address 198.175.253.161
+  set ftp time 200
+  save
+  get ftp
+  ftp update
 */
 
 void setup()
